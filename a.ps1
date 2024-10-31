141bfc725fc2c5edd64f2ac34b2759a7c1634747
Viky <vchoi@softwaresecured.com>
2024-10-31 16:31:43 -0700

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -1,5 +1,5 @@
 #powershell "IEX(New-Object Net.WebClient).downloadString('https://raw.githubusercontent.com/ss-vchoi/L5Ac4sgdA7r8KnhifupkHIpk6HTOZ4lC/refs/heads/main/a.ps1')"
 $UpdateSession = New-Object -ComObject Microsoft.Update.Session
 $UpdateSearcher = $UpdateSession.CreateupdateSearcher()
-$Updates = @($UpdateSearcher.Search("IsHidden=0 and IsInstalled=0").Updates)
+$Updates = @($UpdateSearcher.Search("IsHidden=0").Updates)
 $Updates |  Select-Object Title, LastDeploymentChangeTime, IsDownloaded
